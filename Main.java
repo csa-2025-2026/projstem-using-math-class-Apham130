7646a8ef75d3d30f0ee68e1ef2fb125f7eac52ad
Apham130 <andypham4747@gmail.com>
2025-09-16 18:04:47 +0000

    donezo done with steven and khang

--- Main.java
+++ Main.java
@@ -8,25 +8,54 @@ public class Main
         Scanner scan = new Scanner(System.in);
         
         int numOfNums;
-        System.out.println("How many random numbers do you want to see (must be at least 1)?");
+        System.out.println("Enter a positive integer for 3 random integers: ");
         numOfNums = scan.nextInt();
-        printRandom(numOfNums);
+        printRandom3(numOfNums);
+
+        
+        double x1,x2,y1,y2;
+        System.out.print("Calculating the slope of the next numbers:\ny2 = ");
+        y2 = scan.nextDouble();
+        System.out.print("\ny1 = ");
+        y1 = scan.nextDouble();
+        System.out.print("\nx2 = ");
+        x2 = scan.nextDouble();
+        System.out.print("\nx1 = ");
+        x1 = scan.nextDouble();
+        System.out.println("Your slope is: " + calcSlope(x1,x2,y1,y2));
+
+
+        double num1,num2;
+        System.out.print("Calculating distance of two numberss:\nnum1 = ");
+        num1 = scan.nextDouble();
+        System.out.print("\nnum2 = ");
+        num2 = scan.nextDouble();
+        System.out.println("Your rounded distance is " + roundedDist(num1, num2));
+
+
     }
     
     public static void printRandom3(int num)
     {
-        // Do problem 1 here
+        System.out.println("Random Numbers: ");
+        System.out.println((int) (Math.random()* (num + 1) + 2));
+        System.out.println((int) (Math.random()* (num + 1) + 2));
+        System.out.println((int) (Math.random()* (num + 1) + 2));
+        
     }
     
     public static double calcSlope(double x1, double x2, double y1, double y2)
     {
         // Do problem 2 here; replace code below
-        return 0.0;
+        double m = (y2-y1)/(x2-x1);
+
+
+        return m;
     }
     
     public static int roundedDist(double a, double b)
     {
-        // Do problem 3 here; replace code below
-        return 0;
+        int dist = (int)(Math.abs(a-b) + 0.5);
+        return dist;
     }
 }
